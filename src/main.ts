dd3d5c7264e711171dc057346ae3cc019e96ff0c
Jens Marchewka <jens.marchewka@googlemail.com>
2022-07-14 23:52:23 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,24 @@ router.use(express.urlencoded({ extended: false }));
 /** Takes care of JSON data */
 router.use(express.json());
 
-router.use(cors);
+//router.use(cors);
+
+/** RULES OF OUR API */
+router.use((req, res, next) => {
+    // set the CORS policy
+    res.header('Access-Control-Allow-Origin', '*');
+    // set the CORS headers
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+    // set the CORS method headers
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 /** Routes */
 router.use('/', registerRoutes);
-router.use("/", proxyRoutes);
 
 /** Error handling */
 router.use((req, res, next) => {
