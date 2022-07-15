6ea282e91cf9a1c599177d85ab26c68a13a11714
Jens Marchewka <jens.marchewka@googlemail.com>
2022-07-15 21:41:46 +0200

    Axios is a little bit bad. tbh

--- src/controller/proxy.ts
+++ src/controller/proxy.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as fs from 'fs';
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const fsp = fs.promises;
 
@@ -13,9 +13,26 @@ interface ProxyRequest {
 // update Register
 const proxy = async (req: Request, res: Response, next: NextFunction) => {
     const p = req.body as ProxyRequest;
-    const result = await axios.request({ method: p.method, url: p.target, data: p.payload });
-    return res.status(200).json(
-        result.data
+
+    let response: AxiosResponse<any, any> | undefined;
+
+    try {
+        console.log("1");
+        response = await axios.request({ method: p.method, url: p.target, data: p.payload, timeout: 2000 });         
+        console.log("2");
+    } catch (err: unknown | AxiosError) {
+        if (axios.isAxiosError(err) && err.response)
+        {
+            response = err.response;
+        }
+    }
+    
+    if (!response) {
+        return res.status(500);
+    }
+
+    return res.status(response.status).json(
+        response.data
     );
 };
 
